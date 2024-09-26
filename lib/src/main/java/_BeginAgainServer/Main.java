43c596655193aec4bce27884caeff2e5e01b7ff2
Sonwonill <tlatmsdkqk@gmail.com>
2024-09-26 11:32:27 +0900

    :tada: Feat  : "첫 커밋"

---
+++ lib/src/main/java/_BeginAgainServer/Main.java
@@ -0,0 +1,10 @@
+package _BeginAgainServer;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is Raspberry PI Java Server");
+	}
+
+}
